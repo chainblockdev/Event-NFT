12c1855c5199b5042198890cdae1f204764ef8c5
sepool7 <rajaee.sepideh@gmail.com>
2024-08-04 02:49:43 +0200

    profile  commit

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAuth0 } from '../components/Auth0Provider'; // Adjust path if necessary
+import './Profile.css'; // Import the CSS file
 
 const Profile = () => {
   const { user, isAuthenticated, logout } = useAuth0();
@@ -9,12 +10,14 @@ const Profile = () => {
   }
 
   return (
-    <div>
-      <h1>Profile</h1>
-      <p>Name: {user.name}</p>
-      <img src={user.picture} alt="Profile" />
-      <p>Email: {user.email}</p>
-      <button onClick={() => logout({ returnTo: window.location.origin })}>Logout</button>
+    <div className="profile-container">
+      <div className="profile-card">
+        <h1 className="profile-title">Profile</h1>
+        <img src={user.picture} alt="Profile" className="profile-image" />
+        <p className="profile-name">Name: {user.name}</p>
+        <p className="profile-email">Email: {user.email}</p>
+        <button onClick={() => logout({ returnTo: window.location.origin })} className="profile-logout-button">Logout</button>
+      </div>
     </div>
   );
 };
